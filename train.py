2ebc511cc3ceb0d6a3ba9dbfee7766739a687c01
mgrankin <mv.grankin@gmail.com>
2019-09-13 15:39:22 +0300

    fix for bigger models (774M)

--- train.py
+++ train.py
@@ -80,7 +80,7 @@ def main():
         raise ValueError(
             "Can't get samples longer than window size: %s" % hparams.n_ctx)
 
-    if args.model_name == '345M':
+    if args.model_name != '117M':
         args.memory_saving_gradients = True
         if args.optimizer == 'adam':
             args.only_train_transformer_layers = True
